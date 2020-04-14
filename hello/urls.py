67feed3c203a168f433431c22b7f87dc504aef42
Rohan Battula <rohanbattula@gmail.com>
2020-04-13 21:44:13 -0700

    oops

--- hello/urls.py
+++ hello/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     #path('party/', views.index, name='/'),
     path('', views.index, name='index'),
     #path('signup/',views.db,name='register')
-    path('new/',views.db,name='party_new')
+    path('new/',views.party_new,name='party_new')
     #path('edit/',views.db,name='party_new')
 
     #path('admin/', admin.site.urls),
