5e4e63cb224c001c04575042fc78b08462760841
Rohan Battula <rohanbattula@gmail.com>
2020-05-19 21:30:36 -0700

    fixing models

--- hello/models.py
+++ hello/models.py
@@ -6,11 +6,11 @@ class Party(models.Model):
     # put characteristics of party here
     name = models.CharField(max_length = 100, unique=True)
     address = models.CharField(max_length = 1000,null=True)
-    #status = models.BooleanField(null=True)
+    status = models.BooleanField(null=True)
     distance = models.DecimalField(decimal_places = 2, max_digits = 4,null=True)
-    #entryFee = models.IntegerField(null=True)
-    #dateTime = models.DateTimeField(auto_now_add=True, null=True, blank=True)
-    #guysAllowed = models.BooleanField(null=True)
+    entryFee = models.IntegerField(null=True)
+    dateTime = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    guysAllowed = models.BooleanField(null=True)
     #photo = models.ImageField(upload_to="party/", null=True, blank=True)
 
     def __str__(self):
--- hello/views.py
+++ hello/views.py
@@ -42,7 +42,6 @@ def party_new(request):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        print(s.errors)
         return Response(request.data, status=status.HTTP_400_BAD_REQUEST)
 
 
